d3038ef2f1dd910149b3af745450c4b2c8a0a489
Brandon Hunt <bhunt@KRSDEVBHUNT.local>
2022-02-08 10:35:55 -0700

    pylint test - pylint long line warning

--- hello.py
+++ hello.py
@@ -1,2 +1,2 @@
-""" This program prints Hello, world! """
+""" This program prints Hello, world! This is a very long line. Hello, world! This is a very long line. Hello, world! This is a very long line. Hello, world! This is a very long  line. """
 print('Hello, world!')
